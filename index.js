5b09e4a8020e2882347d931519feffd698c7078e
faniel ghirmay <fanifanu@gmail.com>
2017-10-03 11:13:45 -0700

    added basic sync for new connections

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var curDelta = null;
+var numberOfUsers = 0;
+var newConnectionQueue = [];
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -12,10 +16,7 @@ app.get('/doc', function(req, res){
 
 io.on('connection', function(socket){
 	console.log('a user got connected');
-  socket.on('chat message', function(msg){
-    console.log('i got the ', msg);
-    io.emit('ping', 'right back at you');
-  });
+  
   socket.on('doc-change', function(deltas){
     user = deltas.user;
     delta = deltas.delta;
@@ -25,8 +26,34 @@ io.on('connection', function(socket){
     console.log('old');
     io.emit('delta', {'user':user, 'data':delta});
   });
+
+
+  socket.on('load-get', function(delta){
+    console.log('got the sync data');
+    curDelta = delta;
+    var size = newConnectionQueue.length;
+    console.log('updating ' + size + ' sockets');
+    for(var i=0; i < size; i+=1){ // consume all waiting new clients
+      var clientSocket = newConnectionQueue.shift();
+      clientSocket.emit('connection', delta); 
+    }
+  });
+
+  if(curDelta != null || numberOfUsers != 0){
+    console.log('sync new user');
+    io.sockets.emit('get', '');
+    newConnectionQueue.push(socket);
+  //  socket.emit('connection', curDelta);
+  //  io.sockets.emit('connection', curDelta);
+  }else if(numberOfUsers == 0){
+    console.log('first user');
+    io.sockets.emit('first-connection', '');
+    numberOfUsers += 1;
+  }
 });
-;
+
+
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
